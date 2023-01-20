f9ef680d487fb99274ed4da640898f5776aab169
NurullahSanY <nurcuet18@gmail.com>
2023-01-20 12:28:35 +0600

    Update index.php
    contacts us page start

--- index.php
+++ index.php
@@ -151,12 +151,21 @@
         </div>
       </div>
 
-
     </div> 
   </div>
+
+
+  <!-- Contact us page -->
+  <div class="text-center m-auto mt-4 p-2">
+    <P class="catagory">Contact Us</P>
+  </div>
+
+
+
+
+
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<!-- Front end Work of Nurullah, don't rwrite it -->
-
-
 </body>
 </html>
