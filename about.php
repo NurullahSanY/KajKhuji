a32ff2637a04078561de72c5005a779f17a292ef
NurullahSanY <nurcuet18@gmail.com>
2023-02-04 12:56:03 +0600

    Update about.php

--- about.php
+++ about.php
@@ -54,7 +54,7 @@ include "connection.php";
 
   </div>
 
-
+ <!-- footer -->
 
   <footer class=" bg-dark text-white pt-5 pb-4">
   <div class="container text-center text-md-left">
@@ -133,21 +133,13 @@ include "connection.php";
                 <a href="" class="btn-floating btn-sm text-white" style="font-size: 23px;"> <i class="fa-brands fa-linkedin"></i></i></i></a>
               </li>
             </ul>
-
           </div>
-
         </div>
-
       </div>
-
     </div>
-
   </div>
-
 </footer>
 
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
 </body>
 </html>
